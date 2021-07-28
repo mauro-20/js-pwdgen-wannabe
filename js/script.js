b3b677815e4f00ad3c608fac45bc9f6b1f47f768
Mauro Tomasoni <mauro.tom.vn@gmail.com>
2021-07-28 21:07:49 +0700

    add style

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-var firstName = prompt('Inserisci il tuo nome')
-var lastName = prompt('Inserisci il tuo cognome')
-var color = prompt("ciao " + firstName + ", qual'è il tuo colore preferito?")
-var password = firstName + lastName + color + 40
-document.getElementById('psw').innerHTML = password
+var firstName = prompt('Insert your name')
+var lastName = prompt('Insert your last name')
+var color = prompt("Hi " + firstName + ", what's your favourite color?")
+var password = firstName + lastName + color + '40'
+document.getElementById('psw').innerHTML += password
